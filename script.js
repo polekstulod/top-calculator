19481d9e6ac7b8e887e4ea2b0eff3b2346e9067d
James Paul Tulod <paultulod@pm.me>
2022-10-21 16:08:35 +0800

    Add button event listener

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
 
+const buttons = document.querySelectorAll('.btn')
+const total = document.querySelector('#total')
+
+let firstValue = 0
+let secondValue = 0
+let operator = ''
+let result = 0
+
 function operate(op, a, b) {
   switch (op) {
     case '+':
@@ -15,3 +23,9 @@ function operate(op, a, b) {
       return divide(a, b)
   }
 }
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    total.textContent += button.getAttribute('value').trim()
+  })
+})
