a4a2d7fa6558181e5a4644be1dd4eb173604ab98
James Paul Tulod <paultulod@pm.me>
2022-10-21 10:15:26 +0800

    Add operate function

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const add = (a, b) => a + b
 const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
+
+function operate(op, a, b) {
+  switch (op) {
+    case '+':
+      return add(a, b)
+    case '-':
+      return subtract(a, b)
+    case '*':
+      return multiply(a, b)
+    case '/':
+      return divide(a, b)
+  }
+}
